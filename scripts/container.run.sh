99323300dcfeebe6185ca70db024075d2ce880b8
Nguyen Nhat Anh <nhatanhcd2169@yahoo.com>
2025-11-20 01:26:17 +0700

    Mount SSH keys for homonculus k3s bootstrap <master/worker>

--- scripts/container.run.sh
+++ scripts/container.run.sh
@@ -10,6 +10,7 @@ $CONTAINER_EXEC run \
     --network=host \
     -v /var/run/libvirt/libvirt-sock:/var/run/libvirt/libvirt-sock:Z \
     -v /var/lib/libvirt:/var/lib/libvirt:Z \
+    -v ~/.ssh:/root/.ssh:ro \
     -v $(pwd)/homonculus.yaml:/app/homonculus/homonculus.yaml:Z \
     --privileged \
     --entrypoint homonculus \
